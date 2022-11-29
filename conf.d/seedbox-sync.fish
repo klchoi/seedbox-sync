b0cedc6e866539da17767ca992c36091d7287a6c
Choi <khachlangchoi@gmail.com>
2022-11-29 19:34:20 +0700

    add sync script

--- conf.d/seedbox-sync.fish
+++ conf.d/seedbox-sync.fish
@@ -1,4 +1,6 @@
-function _seedbox_sync_install -e seedbox-sync_install -e seedbox-sync_update
+function _seedbox_sync_install -e seedbox-sync_install
   abbr -aU s 'tmux new-window -n sync sync'
   abbr -aU l 'logs'
+
+  echo '0 0 * * * $HOME/.local/bin/tmux new-tab -n sync $HOME/.config/fish/conf.d/sync.sh' crontab -u $USER -
 end
---
+++ conf.d/sync.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+RCLONE_BIN=$HOME/.local/bin/rclone
+RCLONE_CONF=$HOME/.rclone.conf
+RCLONE_LOG=$HOME/gsync.log
+RCLONE_LOCAL_DIR=$HOME/drive/
+RCLONE_REMOTE_DIR=gdrive:/
+
+$RCLONE_BIN \
+    --config $RCLONE_CONF \
+    --log-level INFO \
+    move \
+    --delete-empty-src-dirs \
+    --drive-stop-on-upload-limit \
+    --no-traverse \
+    --order-by name \
+    --stats=10s \
+    --stats-file-name-length 0 \
+    --exclude='**/.**' \
+    --exclude='/.**' \
+    --exclude='**.mp4' \
+    --exclude='**.wmv' \
+    --exclude='**.mov' \
+    --exclude='**.avi' \
+    --exclude='**.mkv' \
+    --exclude='**.flv' \
+    --exclude='**.f4v' \
+    --transfers=8 \
+    $RCLONE_LOCAL_DIR \
+    $RCLONE_REMOTE_DIR \
+    2>&1 | tee $RCLONE_LOG && \
+$RCLONE_BIN \
+    --config $RCLONE_CONF \
+    --log-level INFO \
+    move \
+    --delete-empty-src-dirs \
+    --drive-stop-on-upload-limit \
+    --no-traverse \
+    --order-by name \
+    --stats=10s \
+    --stats-file-name-length 0 \
+    --exclude='**/.**' \
+    --exclude='/.**' \
+    --transfers=1 \
+    $RCLONE_LOCAL_DIR \
+    $RCLONE_REMOTE_DIR \
+    2>&1 | tee $RCLONE_LOG
