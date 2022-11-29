56e99aa014d30ae8a8122afee9b323931f6a1652
klchoi <66675595+klchoi@users.noreply.github.com>
2022-11-30 05:38:12 +0700

    Update seedbox-sync.fish

--- conf.d/seedbox-sync.fish
+++ conf.d/seedbox-sync.fish
@@ -2,5 +2,5 @@ function _seedbox_sync_install -e seedbox-sync_install
   abbr -aU s 'tmux new-window -n sync sync'
   abbr -aU l 'logs'
 
-  echo '0 0 * * * $HOME/.config/fish/conf.d/sync.sh' | crontab -u $USER -
+  echo '0 */12 * * * $HOME/.config/fish/conf.d/sync.sh' | crontab -u $USER -
 end
