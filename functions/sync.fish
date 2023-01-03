667a2394553880141069873c8d981f9469633dc3
klchoi <66675595+klchoi@users.noreply.github.com>
2023-01-03 21:45:25 +0700

    Update sync.fish

--- functions/sync.fish
+++ functions/sync.fish
@@ -2,7 +2,7 @@ function sync -a cmd
   switch $cmd
     case stop
       pkill -P (cat ~/gsync.lock)
-      pkill --pidfile ~gsync.lock
+      pkill --pidfile ~/gsync.lock
     case copy
       rclone \
         --log-level INFO \
@@ -19,42 +19,6 @@ function sync -a cmd
         gdrive:/ \
         2>&1 | tee ~/gsync.log
     case move \*
-      rclone \
-        --log-level INFO \
-        move \
-        --delete-empty-src-dirs \
-        --drive-stop-on-upload-limit \
-        --no-traverse \
-        --order-by name \
-        --stats=10s \
-        --stats-file-name-length 0 \
-        --exclude='**/.**' \
-        --exclude='/.**' \
-        --exclude='**.mp4' \
-        --exclude='**.wmv' \
-        --exclude='**.mov' \
-        --exclude='**.avi' \
-        --exclude='**.mkv' \
-        --exclude='**.flv' \
-        --exclude='**.f4v' \
-        --transfers=8 \
-        ~/drive/ \
-        gdrive:/ \
-        2>&1 | tee ~/gsync.log && \
-      rclone \
-        --log-level INFO \
-        move \
-        --delete-empty-src-dirs \
-        --drive-stop-on-upload-limit \
-        --no-traverse \
-        --order-by name \
-        --stats=10s \
-        --stats-file-name-length 0 \
-        --exclude='**/.**' \
-        --exclude='/.**' \
-        --transfers=1 \
-        ~/drive/ \
-        gdrive:/ \
-        2>&1 | tee ~/gsync.log
+      ~/.config/fish/conf.d/sync.sh &
   end
 end
